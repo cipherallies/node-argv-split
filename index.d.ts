7ebd6beff9a57b067eee0d8409796db915b72581
Minh Duc Vo <minhducsun2002@gmail.com>
2020-04-05 17:43:14 +0700

    Add TypeScript definition

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+import argvSplit from '.';
+export = split;
+
+/**
+ * Split a argv string into an array of arguments.
+ * @param s String to be parsed.
+ */
+declare function split(s : string): string[];
+
